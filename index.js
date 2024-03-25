7949ce94b5e9e7369b7a38bc71da3ebd293d2b15
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-03-25 12:17:58 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    const lowerCaseLetters = letters.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseLetters));
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+}
